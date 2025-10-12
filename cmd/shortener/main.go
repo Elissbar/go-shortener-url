06b39ab797964cbf9f6ce470a2928d167323708b
Eduard <allen-2002@mail.ru>
2025-10-12 23:03:15 +0300

    iter1: Инкремент 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/Elissbar/go-shortener-url/internal/handler"
+)
+
+func main() {
+	var urls map[string]string
+
+	// Файл используется для хранения коротких ссылок
+	err := handler.GetAllLinks(&urls)
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+
+	// Структуру используем чтобы не открывать файл при каждом POST/GET запросе
+	myHandler := handler.MyHandler{Urls: urls}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", myHandler.CreateShortUrl)
+	mux.HandleFunc("/{id}", myHandler.GetShortUrl)
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+)
+
+type MyHandler struct {
+	Urls map[string]string
+}
+
+func (h *MyHandler) CreateShortUrl(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		urls := h.Urls
+
+		token, err := generateToken()
+		if err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		for _, ok := urls[token]; ok; { // Если такой токен уже есть - генерируем новый
+			token, _ = generateToken()
+		}
+
+		body, err := io.ReadAll(req.Body) // получаем URL для сокращения
+		if err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		urls[token] = string(body)
+
+		scheme := "http://"
+		if req.TLS != nil {
+			scheme = "https://"
+		}
+
+		rw.Header().Set("content-type", "text/plain")
+		rw.WriteHeader(http.StatusCreated)
+		rw.Write([]byte(scheme + req.Host + req.URL.Path + token))
+
+		// Обновляем файл после каждого POST запроса
+		err = updateLinksStore(urls)
+		if err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (h *MyHandler) GetShortUrl(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		urls := h.Urls
+		url, ok := urls[req.URL.Path[1:]]
+		if !ok {
+			rw.WriteHeader(http.StatusNotFound)
+			rw.Write([]byte("Not Found"))
+		} else {
+			http.Redirect(rw, req, url, http.StatusTemporaryRedirect)
+		}
+	}
+}
