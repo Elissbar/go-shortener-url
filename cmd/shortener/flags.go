151b8ea32851daa72272931fbc6b4501a99f89fe
Eduard <allen-2002@mail.ru>
2025-10-26 18:32:09 +0300

    iter5:  добавил получение значений из переменных окружения

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/Elissbar/go-shortener-url/internal/config"
 )
@@ -12,6 +13,13 @@ func parseFlags() *config.Config {
 	flag.StringVar(&serverUrl, "a", "localhost:8080", "<host>:<port>")
 	flag.StringVar(&baseUrl, "b", "http://localhost:8080/", "Base URL for the API. Example: http://localhost:8080/")
 	flag.Parse()
+
+	if osEnvServerAddr := os.Getenv("SERVER_ADDRESS"); osEnvServerAddr != "" {
+		serverUrl = osEnvServerAddr
+	}
+	if osEnvBaseUrl := os.Getenv("BASE_URL"); osEnvBaseUrl != "" {
+		baseUrl = osEnvBaseUrl
+	}
 
 	return config.New(serverUrl, baseUrl)
 }
