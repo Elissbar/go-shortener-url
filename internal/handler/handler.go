f624e087b5cef802958f4f01f76aa3a728b89d97
Eduard <allen-2002@mail.ru>
2025-11-04 13:38:51 +0300

    iter7: add json handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"strings"
 
 	"github.com/Elissbar/go-shortener-url/internal/config"
+	"github.com/Elissbar/go-shortener-url/internal/model"
 	"github.com/Elissbar/go-shortener-url/internal/repository"
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
@@ -22,6 +24,7 @@ func (h *MyHandler) Router() chi.Router {
 
 	r.Use(h.LoggingMiddleware)
 	r.Post("/", h.CreateShortUrl)
+	r.Post("/api/shorten", h.CreateShortUrlJSON)
 	r.Get("/{id}", h.GetShortUrl)
 	r.Get("/", h.GetRoot)
 
@@ -31,6 +34,48 @@ func (h *MyHandler) Router() chi.Router {
 func (h *MyHandler) GetRoot(rw http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodGet {
 		rw.Write([]byte("URL Shortener is running!"))
+	}
+}
+
+func (h *MyHandler) CreateShortUrlJSON(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		rw.Header().Set("Сontent-Type", "application/json")
+
+		token, err := generateToken()
+		if err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(map[string]string{"error": "invalid JSON"})
+			return
+		}
+		for _, ok := h.Storage.Get(token); ok; { // Если такой токен уже есть - генерируем новый
+			token, _ = generateToken()
+		}
+
+		var rq model.Request
+		dec := json.NewDecoder(req.Body)
+
+		if err := dec.Decode(&rq); err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(map[string]string{"error": "invalid JSON"})
+			return
+		}
+
+		h.Storage.Save(token, rq.URL)
+
+		var resp model.Response
+		resp.Result = h.Config.BaseURL + token
+		if !strings.HasSuffix(h.Config.BaseURL, "/") {
+			resp.Result = h.Config.BaseURL + "/" + token
+		}
+
+		rw.WriteHeader(http.StatusCreated)
+
+		enc := json.NewEncoder(rw)
+		if err := enc.Encode(resp); err != nil {
+			http.Error(rw, "Error: "+err.Error(), http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(map[string]string{"error": "invalid JSON"})
+			return
+		}
 	}
 }
 
