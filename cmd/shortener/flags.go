1a36b8b11f155dbd2eb800cf85c7473b100c62f5
Eduard <allen-2002@mail.ru>
2025-11-16 23:37:08 +0300

    iter13: fix

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -20,8 +20,8 @@ func parseFlags() (*config.Config, error) {
 	flag.StringVar(&logLevel, "l", "info", "Log level. Example: info, debug, error")
 	// flag.StringVar(&fileStoragePath, "f", "/tmp/links.json", "File storage path")
 	flag.StringVar(&fileStoragePath, "f", "", "File storage path")
-	flag.StringVar(&databaseAdr, "d", "postgres://postgres:12345@localhost:5432/shorted_links?sslmode=disable", "Database connection string")
-	// flag.StringVar(&databaseAdr, "d", "", "Database connection string")
+	// flag.StringVar(&databaseAdr, "d", "postgres://postgres:12345@localhost:5432/shorted_links?sslmode=disable", "Database connection string")
+	flag.StringVar(&databaseAdr, "d", "", "Database connection string")
 	flag.Parse()
 
 	if cfg.ServerURL == "" {
