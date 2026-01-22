1456c1c4ba302d09e4569797ffd807c64dc28b0b
Eduard <allen-2002@mail.ru>
2026-01-22 22:32:15 +0300

    fix

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 // Кастомные ошибки для обрабоки в хендлерах.
 var (
-	ErrURLExists      error = errors.New("URL already exists") // Ошибка сохранения существующего URL.
-	ErrTokenIsDeleted error = errors.New("token is deleted") // Ошибка возникающая при попытке получения удаленного токена (ссылки).
+	ErrURLExists      error = errors.New("URL already exists")  // Ошибка сохранения существующего URL.
+	ErrTokenIsDeleted error = errors.New("token is deleted")    // Ошибка возникающая при попытке получения удаленного токена (ссылки).
 	ErrTokenNotExist  error = errors.New("token is not exists") // Ошибка при попытке получения несуществующего токена (ссылки).
 )
